b54c5efb3e88ff14436d4eb2b069fa173e7f5f0a
liangxm <liangxm_tom@163.com>
2015-07-20 10:35:18 +0800

    remove the PWHelper branch

--- PWHelper/src/com/lxm/pwhelp/utils/Settings.java
+++
@@ -1,7 +0,0 @@
-package com.lxm.pwhelp.utils;
-
-public class Settings {
-	public static final String SECRET_KEY = "pwhelper_lxm_2015";
-	public static final String strDefaultKey = "PWhelper2015^06";
-	public static final String VERSION = "1.0.4";
-}
